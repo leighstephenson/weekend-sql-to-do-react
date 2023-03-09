85a4767b0553a883acd7296ecdf8aef3399d18dd
Leigh Stephenson <Leighh.stephenson@gmail.com>
2023-03-08 22:40:18 -0600

    added get request

--- server/routes/todo.router.js
+++ server/routes/todo.router.js
@@ -3,8 +3,22 @@ const router = express.Router();
 const pool = require('../modules/pool.js');
 
 // GET
+//! Table name = list, need to build on postico
+router.get('/', (req, res) => {
+    console.log ('GET request');
+    let queryText = 'SELECT * FROM "list";';
+    pool.query(queryText).then((result) => {
+        res.send(result.rows);
+    }).catch ((error) => {
+        console.log (`Error in GET: ${error}`);
+        res.sendStatus(500);
+    });
+});
 
 // POST
+
+
+
 
 // PUT
 
